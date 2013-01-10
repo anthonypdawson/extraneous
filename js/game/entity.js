dd9e7819c7a53a12c7e435bc0f30930fe182bf33
Anthony Dawson <adawson2@vcu.edu>
2013-01-10 16:33:30 -0500

    Proper inheritance

--- js/game/entity.js
+++ js/game/entity.js
@@ -11,16 +11,32 @@ Entity.prototype.move = function(x, y){
 }
 
 function Shape(){
-
+  
 }
 
 Shape.prototype.drawFunction = function(ctx){
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
 }
 
-function Point = Shape.prototype;
+function Point() {
+    Point.call(this);
+}
+
+Point.prototype = new Shape();
 
 Point.prototype.drawFunction = function(ctx){
     ctx.fillRect(this.position.x, this.position.y, 10, 10);
 }
 
+function Line(){
+    Line.call(this);
+    var points = [];
+}
+
+Line.prototype = new Shape();
+
+function Square(){
+    Square.call(this);
+}
+
+Square.prototype = new Shape();
