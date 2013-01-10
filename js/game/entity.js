86d9c6622ff3484f04e586f49aa311f6d2a064e8
ayd <anthonypdawson@gmail.com>
2013-01-10 16:44:42 -0500

    Added more shapes and draw methods

--- js/game/entity.js
+++ js/game/entity.js
@@ -1,8 +1,5 @@
 function Entity() {
     this.position = {x: 0, y: 0};
-    this.height = 0;
-    this.width = 0;
-    this.shape = null;    
 }
 
 Entity.prototype.move = function(x, y){
@@ -11,32 +8,54 @@ Entity.prototype.move = function(x, y){
 }
 
 function Shape(){
-  
+    Shape.call(this);
+    this.name = "Shape";
 }
 
-Shape.prototype.drawFunction = function(ctx){
-    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-}
+Shape.prototype = new Entity();
 
 function Point() {
     Point.call(this);
+    this.height = 0;
+    this.width = 0;
+    this.name = "Point";
 }
 
 Point.prototype = new Shape();
 
 Point.prototype.drawFunction = function(ctx){
-    ctx.fillRect(this.position.x, this.position.y, 10, 10);
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
 }
 
 function Line(){
     Line.call(this);
-    var points = [];
+    var startPoint;
+    var endPoint;
+    this.name = "Line";
 }
 
 Line.prototype = new Shape();
 
-function Square(){
-    Square.call(this);
+Line.prototype.drawFunction = function(ctx){
+    ctx.moveTo(this.startPoint.x, this.startPoint.y);
+    ctx.lineTo(this.endPoint.x, this.endPoint.y);
 }
 
-Square.prototype = new Shape();
+Line.prototype.drawFunction = function(ctx) {
+    $.each(this.points, function(i, p){
+	if (i == 0){
+	    ctx.moveTo(p.x, p.y);
+	} else {
+	    ctx.lineTo(p.x, p.y);
+	    ctx.stroke();
+	    ctx.moveTo(p.x, p.y);
+	}
+    });
+}
+
+function Square(){
+    Square.call(this);
+    this.name = "Square";
+}
+
+Square.prototype = new Point();
